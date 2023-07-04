27bd32604a5287dcd0f7b87a6ae7008e9c85e20e
William de Souza <william.sza@outlook.com>
2023-07-04 14:01:02 -0300

    Fist commit

--- spring-boot/src/main/java/br/com/dicasdeumdev/api/Application.java
+++ spring-boot/src/main/java/br/com/dicasdeumdev/api/Application.java
@@ -3,11 +3,16 @@ package br.com.dicasdeumdev.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.dicasdeumdev.api.domain.User;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		User User = new User(1,"William","william@email.com", "123456");
+		System.out.println(User);
 	}
 
 }
---
+++ spring-boot/src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,28 @@
+package br.com.dicasdeumdev.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String senha;
+
+}
